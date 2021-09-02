a2f71011a5150988b317692bac20be83c0780b98
Thanos Paravantis <thanosparavantis@gmail.com>
2021-09-02 17:53:36 +0300

    Update tailwind purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx,json}', './public/index.html'],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
