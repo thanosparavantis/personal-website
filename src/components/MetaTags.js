ac78e174089a8183f8ff40403010596c2df6ac25
Thanos Paravantis <thanosparavantis@gmail.com>
2021-11-26 12:26:12 +0200

    Updates

--- src/components/MetaTags.js
+++ src/components/MetaTags.js
@@ -3,40 +3,31 @@ import {useMemo} from "react";
 
 export default function MetaTags({title, description, image = null}) {
   const metaUrl = useMemo(() => {
-    return window.location.href
+    return "https://www.thanosparavantis.com" + window.location.pathname
   }, [])
-
-  const domain = useMemo(() => {
-    const url = new URL(metaUrl)
-    return url.protocol + "//" + url.host
-  }, [metaUrl])
 
   const metaTitle = useMemo(() => {
     return `Thanos Paravantis - ${title}`
   }, [title])
 
-  const metaDesc = useMemo(() => {
-    return description
-  }, [description])
-
   const metaImage = useMemo(() => {
-    return domain + (image || "/preview.jpg")
-  }, [domain])
+    return "https://www.thanosparavantis.com" + (image || "/preview.jpg")
+  }, [])
 
   return (
     <Helmet>
       <title>{metaTitle}</title>
       <meta name="title" content={metaTitle}/>
-      <meta name="description" content={metaDesc}/>
+      <meta name="description" content={description}/>
       <meta property="og:type" content="website"/>
       <meta property="og:url" content={metaUrl}/>
       <meta property="og:title" content={metaTitle}/>
-      <meta property="og:description" content={metaDesc}/>
+      <meta property="og:description" content={description}/>
       <meta property="og:image" content={metaImage}/>
       <meta property="twitter:card" content="summary_large_image"/>
       <meta property="twitter:url" content={metaUrl}/>
       <meta property="twitter:title" content={metaTitle}/>
-      <meta property="twitter:description" content={metaDesc}/>
+      <meta property="twitter:description" content={description}/>
       <meta property="twitter:image" content={metaImage}/>
     </Helmet>
   )
