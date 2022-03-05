69e0bfafc352c7452e8530f12b06f79ac4e6c788
Thanos Paravantis <thanosparavantis@gmail.com>
2022-03-05 10:58:05 +0200

    Updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import {useMemo} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFacebook, faGithub, faLinkedin, faTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   const year = useMemo(() => {
@@ -6,8 +8,43 @@ export default function Footer() {
   }, [])
 
   return (
-    <footer className="bg-white border-t shadow p-5 text-gray-600 font-bold md:text-right text-center md:text-xs text-sm">
-      &copy; {year} Thanos Paravantis
+    <footer className="flex flex-col md:flex-row items-center justify-center md:justify-between text-center
+                       gap-5 bg-white border-t shadow p-5 text-gray-600 font-bold md:text-xs text-sm">
+      <div>
+        &copy; {year} Thanos Paravantis
+      </div>
+      <div className="flex flex-wrap gap-3">
+        <a href="https://www.facebook.com/thanosparavantis/"
+           rel="noopener noreferrer"
+           target="_blank"
+           className="text-blue-600 hover:opacity-60">
+          <FontAwesomeIcon icon={faFacebook} size="2x"/>
+        </a>
+        <a href="https://twitter.com/thparavantis"
+           rel="noopener noreferrer"
+           target="_blank"
+           className="text-blue-400 hover:opacity-60">
+          <FontAwesomeIcon icon={faTwitter} size="2x"/>
+        </a>
+        <a href="https://www.linkedin.com/in/thanosparavantis/"
+           rel="noopener noreferrer"
+           target="_blank"
+           className="text-blue-800 hover:opacity-60">
+          <FontAwesomeIcon icon={faLinkedin} size="2x"/>
+        </a>
+        <a href="https://github.com/thanosparavantis"
+           rel="noopener noreferrer"
+           target="_blank"
+           className="text-gray-900 hover:opacity-60">
+          <FontAwesomeIcon icon={faGithub} size="2x"/>
+        </a>
+        <a href="https://www.youtube.com/channel/UCjBLN827xiPCA19MMt_jbmA"
+           rel="noopener noreferrer"
+           target="_blank"
+           className="text-red-700 hover:opacity-60">
+          <FontAwesomeIcon icon={faYoutube} size="2x"/>
+        </a>
+      </div>
     </footer>
   )
 }
