0c583c5df669ef9f852ca76b65cd55d49d51ce38
Thanos Paravantis <thanosparavantis@gmail.com>
2022-04-12 07:33:51 +0300

    Update ThesisPage.js

--- src/pages/ThesisPage.js
+++ src/pages/ThesisPage.js
@@ -19,8 +19,8 @@ export default function ThesisPage() {
   return (
     <PageTemplate>
       <MetaTags
-        title="Project: Thesis - Documentation"
-        description="Applications of NEAT algorithm for automatic gameplay of agents in deterministic and non-deterministic game environments."
+        title="Applications of NEAT algorithm for automatic gameplay of agents in deterministic and non-deterministic game environments"
+        description="Overview of the NEAT algorithm, how it works, design principles and the challenges that come with implementation. We introduce a custom two-dimensional game in Python for two players: blue and red. We lay down the basic rules and structure, in order to create an environment suitable for neuroevolution. Finally, we study five training cases, where the blue and red player are given several tasks that must be achieved through the evolution of neural networks."
       />
       <main className="my-16 flex flex-col gap-7">
         <ThesisAcademicIdentity/>
