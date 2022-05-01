ae5067890b1ece7eb3c0d75b3088d42881a05772
Thanos Paravantis <thanosparavantis@gmail.com>
2022-05-01 18:47:06 +0300

    Updates

--- src/components/ProjectTag.js
+++ src/components/ProjectTag.js
@@ -8,11 +8,11 @@ export default function ProjectTag({tag, active = false, expand = false}) {
     <Link to={active ? "/projects" : `/projects/tag/${tag}`}
           title={`Click here to view all projects related with ${tag}`}
           className={`group flex border rounded text-sm active:text-white
-                      active:bg-green-500 active:border-green-600
-                      ${active ? "text-white bg-green-500 border-green-600" 
+                      active:bg-emerald-500 active:border-emerald-600
+                      ${active ? "text-white bg-emerald-500 border-emerald-600" 
                       : "text-gray-600 bg-gray-50 border-gray-400 hover:border-gray-600"}
                       ${expand && "flex-grow"}`}>
-      <div className={`px-1 border-r ${active ? "border-green-600" : "border-gray-400 group-hover:border-gray-600"}`}>
+      <div className={`px-1 border-r ${active ? "border-emerald-600" : "border-gray-400 group-hover:border-gray-600"}`}>
         {taggedProjects.length}
       </div>
       <div className="px-1">
