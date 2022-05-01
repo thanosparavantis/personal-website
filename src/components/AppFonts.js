b92591dc01e91f169f4d5823d0bc34ceabd214e3
Thanos Paravantis <thanosparavantis@gmail.com>
2022-05-01 22:29:38 +0300

    Updates

--- src/components/AppFonts.js
+++ src/components/AppFonts.js
@@ -3,10 +3,10 @@ import {Helmet} from "react-helmet";
 export default function AppFonts() {
   return (
     <Helmet>
-      <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("./fonts/Lato-Regular.ttf")}/>
-      <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("./fonts/Lato-Italic.ttf")}/>
-      <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("./fonts/Lato-Bold.ttf")}/>
-      <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("./fonts/Lato-Black.ttf")}/>
+      <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("../fonts/Lato-Regular.ttf")}/>
+      <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("../fonts/Lato-Italic.ttf")}/>
+      <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("../fonts/Lato-Bold.ttf")}/>
+      <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("../fonts/Lato-Black.ttf")}/>
     </Helmet>
   )
 }
