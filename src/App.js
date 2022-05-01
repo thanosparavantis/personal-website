434dd62e66742aa47946ac0cad531e7a34158483
Thanos Paravantis <thanosparavantis@gmail.com>
2022-05-01 22:03:55 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
     <BrowserRouter>
       <Helmet>
-        <link rel="preload" as="font" type="font/ttf" href={require("./fonts/Lato-Regular.ttf")}/>
-        <link rel="preload" as="font" type="font/ttf" href={require("./fonts/Lato-Italic.ttf")}/>
-        <link rel="preload" as="font" type="font/ttf" href={require("./fonts/Lato-Bold.ttf")}/>
-        <link rel="preload" as="font" type="font/ttf" href={require("./fonts/Lato-Black.ttf")}/>
+        <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("./fonts/Lato-Regular.ttf")}/>
+        <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("./fonts/Lato-Italic.ttf")}/>
+        <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("./fonts/Lato-Bold.ttf")}/>
+        <link rel="preload" as="font" type="font/ttf" crossOrigin="anonymous" href={require("./fonts/Lato-Black.ttf")}/>
       </Helmet>
       <ScrollToTop/>
       <AppRoutes/>
