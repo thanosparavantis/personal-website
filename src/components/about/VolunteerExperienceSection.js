3a90d6426d6e39a3b81e8414c08262384f026034
Thanos Paravantis <thanosparavantis@gmail.com>
2022-12-02 15:16:25 +0200

    Update VolunteerExperienceSection.js

--- src/components/about/VolunteerExperienceSection.js
+++ src/components/about/VolunteerExperienceSection.js
@@ -11,38 +11,6 @@ export default function VolunteerExperienceSection() {
       </h2>
       <div className="px-5 md:px-8 py-5 border-b">
         <h3 className="mb-1 font-bold text-gray-900">
-          IT Manager
-        </h3>
-        <div className="mb-2 flex flex-col md:flex-row text-gray-700">
-          <p>
-            October 2020 - September 2021
-          </p>
-          <p className="mx-3 hidden md:block">
-            &middot;
-          </p>
-          <p>
-            TEDxUniversityofPiraeus
-          </p>
-        </div>
-        <p className="mb-3 leading-7 text-justify">
-          Responsible for coordinating all IT related work in preparation
-          for the annual TEDx local event with the support of the University of Piraeus.
-          Managed a team of two web developers and supervised the creation of a new website for the organization.
-        </p>
-        <div className="flex flex-col md:flex-row md:items-center items-start">
-          <ExternalLink href="https://www.tedxuniversityofpiraeus.com/">
-            Visit organization website
-          </ExternalLink>
-          <p className="mx-3 hidden md:block">
-            &middot;
-          </p>
-          <ExternalLink href="https://www.ted.com/tedx/events/45617">
-            Visit TED.com event page
-          </ExternalLink>
-        </div>
-      </div>
-      <div className="px-5 md:px-8 py-5">
-        <h3 className="mb-1 font-bold text-gray-900">
           President
         </h3>
         <div className="mb-2 flex flex-col md:flex-row text-gray-700">
@@ -70,6 +38,38 @@ export default function VolunteerExperienceSection() {
         <div className="flex flex-col md:flex-row md:items-center items-start">
           <ExternalLink href="https://pasixeracb.com/">
             Visit club website
+          </ExternalLink>
+        </div>
+      </div>
+      <div className="px-5 md:px-8 py-5">
+        <h3 className="mb-1 font-bold text-gray-900">
+          IT Manager
+        </h3>
+        <div className="mb-2 flex flex-col md:flex-row text-gray-700">
+          <p>
+            October 2020 - September 2021
+          </p>
+          <p className="mx-3 hidden md:block">
+            &middot;
+          </p>
+          <p>
+            TEDxUniversityofPiraeus
+          </p>
+        </div>
+        <p className="mb-3 leading-7 text-justify">
+          Responsible for coordinating all IT related work in preparation
+          for the annual TEDx local event with the support of the University of Piraeus.
+          Managed a team of two web developers and supervised the creation of a new website for the organization.
+        </p>
+        <div className="flex flex-col md:flex-row md:items-center items-start">
+          <ExternalLink href="https://www.tedxuniversityofpiraeus.com/">
+            Visit organization website
+          </ExternalLink>
+          <p className="mx-3 hidden md:block">
+            &middot;
+          </p>
+          <ExternalLink href="https://www.ted.com/tedx/events/45617">
+            Visit TED.com event page
           </ExternalLink>
         </div>
       </div>
